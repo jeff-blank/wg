b7644c0bbbd8fda804f414ddcea4c4b5178153f2
Jeff Blank <jeff-blank@users.noreply.github.com>
2020-03-12 18:57:27 -0700

    new files for login system

---
+++ app/controllers/init.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"github.com/jeff-blank/wg/app/routes"
+	"github.com/revel/revel"
+)
+
+func Authenticate(c *revel.Controller) revel.Result {
+	username, err := c.Session.Get("username")
+	if err != nil {
+		if err.Error() == "Session value not found" {
+			revel.AppLog.Debugf("no username")
+			c.Flash.Error("You are not logged in")
+		} else {
+			revel.AppLog.Debugf("username error: %#v", err)
+			c.Flash.Error(err.Error())
+		}
+		return c.Redirect(routes.Login.Index() + "?back=" + c.Request.URL.Path)
+	} else {
+		revel.AppLog.Debugf("username: %#v", username)
+	}
+	return nil
+}
+
+func init() {
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &Bingos{})
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &Entries{})
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &Hits{})
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &Reports{})
+	//revel.InterceptFunc(Authenticate, revel.BEFORE, &Charts{})
+	//revel.InterceptFunc(Authenticate, revel.BEFORE, &Util{})
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &App{})
+	revel.InterceptFunc(Authenticate, revel.BEFORE, &Testing{})
+}
---
+++ app/controllers/login.go
@@ -0,0 +1,95 @@
+package controllers
+
+import (
+	"crypto/sha256"
+	"fmt"
+
+	"github.com/jeff-blank/wg/app"
+	"github.com/revel/revel"
+)
+
+const (
+	ERR_NO_USER       = "No username specified"
+	ERR_NO_PASS       = "No password specified"
+	ERR_BAD_USER_PASS = "You supplied an incorrect username and/or password."
+	ERR_SYSTEM_ERROR  = "A system error occurred when trying to validate your credentials."
+)
+
+type Login struct {
+	*revel.Controller
+}
+
+func (c Login) Index() revel.Result {
+	backURL := c.Params.Get("back")
+	return c.Render(backURL)
+}
+
+func (c Login) Login() revel.Result {
+	var (
+		errStr string
+		user   string
+		pass   string
+		dbPass string
+	)
+
+	// check that form vars are present
+	if _, ok := c.Params.Form["user"]; !ok {
+		errStr = ERR_NO_USER
+	} else if _, ok := c.Params.Form["passwd"]; !ok {
+		errStr = ERR_NO_PASS
+	}
+	if errStr != "" {
+		c.Flash.Error(errStr)
+		return c.Redirect(App.Index)
+	}
+
+	// check that form vars are populated
+	user = c.Params.Form["user"][0]
+	pass = c.Params.Form["passwd"][0]
+	if user == "" {
+		errStr = ERR_NO_USER
+	} else if pass == "" {
+		errStr = ERR_NO_PASS
+	}
+	if errStr != "" {
+		c.Flash.Error(errStr)
+		return c.Redirect(App.Index)
+	}
+
+	pwHash := ""
+	for _, b := range sha256.Sum256([]byte(pass)) {
+		pwHash += fmt.Sprintf("%02x", b)
+	}
+
+	err := app.DB.QueryRow(`select password from auth where username=$1`, user).Scan(&dbPass)
+	if err != nil {
+		if err.Error() == `sql: no rows in result set` {
+			errStr = ERR_BAD_USER_PASS
+		} else {
+			revel.AppLog.Errorf("%#v", err)
+			errStr = ERR_SYSTEM_ERROR
+		}
+	} else if pwHash != dbPass {
+		errStr = ERR_BAD_USER_PASS
+	}
+	if errStr != "" {
+		c.Flash.Error(errStr)
+		return c.Redirect(App.Index)
+	}
+
+	c.Session.Set("username", user)
+
+	backURL := c.Params.Get("back")
+	if backURL == "" {
+		return c.Redirect(App.Index)
+	}
+	return c.Redirect(backURL)
+}
+
+func (c Login) Logout() revel.Result {
+	c.Session.Del("username")
+	c.Flash.Success("You have been logged out.")
+	return c.Redirect(Login.Index)
+}
+
+// vim:foldmethod=marker:
