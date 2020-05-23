de77c65f7152a626bebbb00b9c1e0f4c96d9453f
Jeff Blank <jeff-blank@users.noreply.github.com>
2020-05-23 07:21:32 -0700

    fix issue loading county SVG in new tab

--- jq/newWin-g.go
+++ jq/newWin-g.go
@@ -10,8 +10,15 @@ var jq = jquery.NewJQuery
 func main() {
 
 	jq(".newWin").On(jquery.CLICK, func(e jquery.Event) {
+		var linkObj jquery.JQuery
+
+		if e.Target.String() == "[object HTMLImageElement]" {
+			linkObj = jq(e.Target).Parent()
+		} else {
+			linkObj = jq(e.Target)
+		}
 		e.PreventDefault()
-		href := jq(e.Target).Attr("href")
+		href := linkObj.Attr("href")
 		js.Global.Get("window").Call("open", href, "", "")
 	})
 
